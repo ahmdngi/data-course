b5a72bebdb3b6da2b1321510727c88c4df80bcdd
ahnasr <ahnasr@taltech.ee>
2023-03-21 17:54:42 +0200

    beforel lab

--- h3/src/LongStack.java
+++ h3/src/LongStack.java
@@ -1,4 +1,5 @@
 //import java.util.Arrays;
+
 //https://www.geeksforgeeks.org/stack-class-in-java/
 //https://stevepesce879.medium.com/what-is-a-stack-and-how-to-create-one-in-java-e2c0f2852eb7
 //https://www.youtube.com/watch?v=fptlqsesjxY&t=48s
@@ -37,6 +38,7 @@ public class LongStack {
          current = current.next;
          clonedCurrent = clonedNode;
       }
+
       return clonedStack;
 
 
@@ -63,12 +65,13 @@ public class LongStack {
          throw new RuntimeException("Stack Underflow");
       }
       long data = top.data;
-         top = top.next;
+      top = top.next;
       size --;
       return data;
    }
    public void op (String s) {
       if (this.size() < 2) {
+
          throw new RuntimeException("Stack Underflow" + s);
       }
       long b = pop();
@@ -142,11 +145,14 @@ public class LongStack {
    public static long interpret (String pol) {
       LongStack stack = new LongStack();
       String[] tokens = pol.split(" ");
+
       for (String token : tokens) {
+
          try {
             if (isNull(token)) {
                throw new RuntimeException("Empty Token " + pol);
             } else if (isWhite(token)) {
+
                throw new RuntimeException("White Space Token " + pol);
             } else if (token.contains("\t")) {
                token = token.replace("\t", "");
@@ -157,74 +163,36 @@ public class LongStack {
                throw new RuntimeException("an illegal symbol " + pol);
             }
          } catch (RuntimeException e) {
+
             continue;
          }
+
          if (isNumber(token)) {
             stack.push(Long.parseLong(token));
          }
          else if (isOperator(token)) {
-            if (stack.size() < 1) {
-              throw new RuntimeException("Not enough numbers to perform operation " + pol);
+            if (stack.size() < 2) {
+
+               throw new RuntimeException("Not enough numbers to perform operation " + pol);
             }
+            long op2 = stack.pop();
+            long op1 = stack.pop();
             switch (token) {
-               case "+": {
-                  long op2 = stack.pop();
-                  long op1 = stack.pop();
+               case "+":
                   stack.push(op1 + op2);
                   break;
-               }
-               case "-": {
-                  long op2 = stack.pop();
-                  long op1 = stack.pop();
+               case "-":
                   stack.push(op1 - op2);
                   break;
-               }
-               case "*": {
-                  long op2 = stack.pop();
-                  long op1 = stack.pop();
+               case "*":
                   stack.push(op1 * op2);
                   break;
-               }
-               case "/": {
-                  long op2 = stack.pop();
-                  long op1 = stack.pop();
+               case "/":
                   if (op2 == 0) {
                      throw new RuntimeException("Division by zero" + pol);
                   }
                   stack.push(op1 / op2);
                   break;
-               }
-               case "SWAP": {
-                  if (stack.size() < 2) {
-                     throw new RuntimeException("Not enough elements to perform SWAP operation " + pol);
-                  }
-                  long op2 = stack.pop();
-                  long op1 = stack.pop();
-                  stack.push(op2);
-                  stack.push(op1);
-                  break;
-               }
-               case "ROT": {
-                  if (stack.size() < 3) {
-                     throw new RuntimeException("Not enough elements to perform ROT operation " + pol);
-                  }
-                  long top = stack.pop();
-                  long mid = stack.pop();
-                  long bot = stack.pop();
-                  stack.push(mid);
-                  stack.push(top);
-                  stack.push(bot);
-                  break;
-               }
-               case "DUP": {
-                  if (stack.size() < 1) {
-                     throw new RuntimeException("Not enough elements to perform DUP operation " + pol);
-                  }
-                  long op = stack.pop();
-                  stack.push(op);
-                  stack.push(op);
-                  break;
-               }
                default:
                   throw new RuntimeException("Invalid operator: " + pol);
             }
@@ -234,11 +202,15 @@ public class LongStack {
          }
       }
       if (stack.stEmpty() ) {
+
          throw new RuntimeException("Not enough numbers"  + pol);
 
       }
+
       long result = stack.pop();
+
       if (!stack.stEmpty()){
+
          throw new RuntimeException("Too many Numbers " + pol);
 
       }
@@ -255,52 +227,11 @@ public class LongStack {
       return s.matches("^$");
    }
    private static boolean isOperator(String s) {
-      return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("SWAP") || s.equals("ROT") || s.equals("DUP") ;
+      return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
 
    }
    private static boolean isIllegal(String s) {
-      return s.matches("[a-zA-Z]+") && !s.equals("SWAP") && !s.equals("ROT") && !s.equals("DUP");
+      return s.matches("[a-zA-Z]+");
    }
 
 }
-
-//System.out.println("Token Contains Tab " + Arrays.toString(tokens));
-//token = token.replace("[a-zA-Z]+", "");
-//System.out.println("an illegal symbol " + pol);
-//System.out.println("Too many Numbers " + pol);
-/*
-         catch (RuntimeException e) {
-            // handle the error and continue processing
-            System.err.println("Error: " + e.getMessage());
-            continue;
-         }
-         if (isNull(token)) {
-            stack.pop(Long.parseLong(token));
-            throw new RuntimeException("Null Token " + token);
-            continue;
-         }
-         else if (isWhite(token)) {
-            throw new RuntimeException("White Space" + token);
-         }
-         else if (token.contains("\t")) {
-               token = token.replace("\t", "");
-               stack.push(Long.parseLong(token));
-            throw new RuntimeException("White Space" + token);
-         }
-         */
-/*
-   private static boolean isTab(String s) {
-      return s.matches("\\t+");
-   }
-*/
-      /*
-      if (stack.size() != 1) {
-         throw new RuntimeException("Too many operands");
-      }
-      */
-      /*
-      Node current=this.top;
-      Node newTop;
-      newTop = new Node(current.data);
-      clonedstack.top = newTop;
-      */
